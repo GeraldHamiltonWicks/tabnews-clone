eb5647330cdc9e9fbe0a43d7956d59dff51ad208
Gerald Hamilton Wicks <88902615+GeraldHamiltonWicks@users.noreply.github.com>
2026-02-18 21:47:46 +0000

    feat: adding cat page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 const Home = () => {
-    return <h1>Teste 2</h1>
+    return <div className='home'>
+    <h1 style={{ textAlign: 'center', margin: '64px' }}>Fala galera do curso.dev, ces tão baoo ??</h1>
+    <img src="https://i.pinimg.com/originals/88/14/9b/88149b0400750578f4d07d9bc3fb0fee.gif" />
+    <img src="https://cdn.dicionariopopular.com/imagens/numero-nove.gif" />
+    <img src="https://cdn3.emoji.gg/emojis/17334-cat-dance.gif" />
+    <img src="https://www.portaldodog.com.br/wp-content/uploads/2013/11/cachorro-engracado-vidro.gif" />
+    <img src="https://i.pinimg.com/originals/7e/24/05/7e2405798d521fd461a1dc8a36dc4a79.gif" />
+    </div>
 };
 
 export default Home;
