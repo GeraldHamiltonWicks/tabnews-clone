7af461fa4240a2dacbe7fafb7602108d509b5dce
Gerald Hamilton Wicks <88902615+GeraldHamiltonWicks@users.noreply.github.com>
2026-05-05 22:43:12 +0000

    feat: adding more status properties

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,12 +2,16 @@ import database from "infra/database.js";
 
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const postgresVersionQuery = await database.query("SELECT version();");
+  const maxDbConnectionsQuery = await database.query("SHOW max_connections;");
+  const dbConnextionsUsed = await database.query(
+    "SELECT count(*) FROM pg_stat_activity;",
+  );
 
   response.status(200).send({
     updated_at: updatedAt,
-    // TODO: implement those entries and also add the unit tests for it
-    postgres_version: null,
-    max_db_connections: 0,
-    db_connections_used: 0,
+    postgres_version: postgresVersionQuery.rows?.[0]?.version,
+    max_db_connections: maxDbConnectionsQuery?.rows?.[0]?.max_connections,
+    db_connections_used: dbConnextionsUsed.rows?.[0]?.count,
   });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,6 +3,10 @@ const PORT = "http://localhost:3000";
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch(`${PORT}/api/v1/status`);
   expect(response.status).toBe(200);
+});
+
+test("GET to /api/v1/status should return updated_at", async () => {
+  const response = await fetch(`${PORT}/api/v1/status`);
 
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
@@ -10,4 +14,29 @@ test("GET to /api/v1/status should return 200", async () => {
   // Good way to check isDate
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("GET to /api/v1/status should return postgres_version", async () => {
+  const response = await fetch(`${PORT}/api/v1/status`);
+
+  const responseBody = await response.json();
+  expect(responseBody.postgres_version).toBeDefined();
+});
+
+test("GET to /api/v1/status should return max_db_connections", async () => {
+  const response = await fetch(`${PORT}/api/v1/status`);
+
+  const responseBody = await response.json();
+  expect(responseBody.max_db_connections).toBeDefined();
+  expect(Number(responseBody.max_db_connections)).toBeGreaterThan(0);
+});
+
+test("GET to /api/v1/status should return db_connections_used", async () => {
+  const response = await fetch(`${PORT}/api/v1/status`);
+
+  const responseBody = await response.json();
+  expect(responseBody.db_connections_used).toBeDefined();
+  expect(Number(responseBody.db_connections_used)).toBeLessThanOrEqual(
+    Number(responseBody.max_db_connections),
+  );
 });
