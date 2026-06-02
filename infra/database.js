f5bf474c9cca19602299cd9680110ea19059de44
Gerald Hamilton Wicks <88902615+GeraldHamiltonWicks@users.noreply.github.com>
2026-06-02 21:51:20 +0000

    refactor: disable ssl for local development

--- infra/database.js
+++ infra/database.js
@@ -9,14 +9,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: true,
-  });
-  console.log("credentials: ", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    //password: process.env.POSTGRES_PASSWORD,
+    ssl: !process.env.NODE_ENV === "development",
   });
 
   try {
